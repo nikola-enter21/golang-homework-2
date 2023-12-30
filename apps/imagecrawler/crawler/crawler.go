a32e904ead41b2a6e2851810b0bc55c91184b72b
nikola-enter21 <nikola@enter21.io>
2023-12-30 17:14:53 +0200

    Banica hard push

--- apps/imagecrawler/crawler/crawler.go
+++ apps/imagecrawler/crawler/crawler.go
@@ -2,91 +2,116 @@ package crawler
 
 import (
 	"awesomeProject1/apps/imagecrawler/extracter"
-	"awesomeProject1/apps/imagecrawler/queue"
 	"awesomeProject1/db"
 	"context"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 )
 
 const maxDepth = 0
 
+type job struct {
+	URL   string
+	Depth int
+}
+
 type Crawler struct {
-	urlQueue         *queue.URLQueue
+	jobs             chan job
+	workers          int
 	db               db.DB
-	timeout          uint
+	timeout          int
 	imagesFolderName string
 	visited          *sync.Map
 	wg               *sync.WaitGroup
 }
 
-func NewCrawler(urlQueue *queue.URLQueue, db db.DB, timeout uint, imagesFolderName string, visited *sync.Map, wg *sync.WaitGroup) *Crawler {
+func NewCrawler(db db.DB, workers int, timeout int, imagesFolderName string) *Crawler {
 	return &Crawler{
-		urlQueue:         urlQueue,
+		jobs:             make(chan job),
+		workers:          workers,
 		db:               db,
 		timeout:          timeout,
 		imagesFolderName: imagesFolderName,
-		visited:          visited,
-		wg:               wg,
+		visited:          &sync.Map{},
+		wg:               &sync.WaitGroup{},
 	}
 }
 
-func (c *Crawler) Crawl() {
-	defer c.wg.Done()
+func (c *Crawler) enqueue(j job) {
+	c.wg.Add(1)
+	c.jobs <- j
+}
 
-	for {
-		URL, ok := c.urlQueue.Dequeue()
-		if !ok {
-			return
-		}
-
-		_, alreadyVisited := c.visited.LoadOrStore(URL.URL, true)
-		if alreadyVisited {
-			continue
-		}
-
-		pageCtx, pageCancel := context.WithTimeout(context.Background(), time.Minute*time.Duration(c.timeout))
-		fmt.Println("Crawling URL", URL.URL, "Depth:", URL.Depth)
-		resp, err := http.Get(URL.URL)
-		if err != nil {
-			fmt.Println("Fetching error:", err)
-			resp.Body.Close()
-			pageCancel()
-			continue
-		}
-		b, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Reading body: ", err)
-			resp.Body.Close()
-			pageCancel()
-			continue
-		}
-		resp.Body.Close()
-
-		err = extracter.DownloadImages(pageCtx, c.imagesFolderName, c.db, b, URL.URL)
-		if err != nil {
-			fmt.Println("DownloadImages Err", err)
-		}
-
-		if URL.Depth >= maxDepth {
-			pageCancel()
-			continue
-		}
-
-		links, err := extracter.ExtractLinks(pageCtx, b, URL.URL)
-		if err != nil {
-			fmt.Println("ExtractLinks Err", err)
-			pageCancel()
-			continue
-		}
-
-		for _, link := range links {
-			c.urlQueue.Enqueue(link, URL.Depth+1)
-		}
-
-		pageCancel()
+func (c *Crawler) Start(startingLinks []string) {
+	if err := os.MkdirAll("apps/imagecrawler/"+c.imagesFolderName, os.ModePerm); err != nil {
+		fmt.Printf("Error creating image directory: %v\n", err)
+		os.Exit(1)
 	}
+
+	for i := 0; i < c.workers; i++ {
+		go func() {
+			for j := range c.jobs {
+				c.runJob(j)
+				c.wg.Done()
+			}
+		}()
+	}
+
+	for _, link := range startingLinks {
+		c.enqueue(job{URL: link, Depth: 0})
+	}
+	c.wg.Wait()
+	close(c.jobs)
+}
+
+func (c *Crawler) runJob(j job) {
+	_, alreadyVisited := c.visited.LoadOrStore(j.URL, true)
+	if alreadyVisited {
+		return
+	}
+
+	pageCtx, pageCancel := context.WithTimeout(context.Background(), time.Minute*time.Duration(c.timeout))
+	fmt.Println("Crawling URL", j.URL, "Depth:", j.Depth)
+	resp, err := http.Get(j.URL)
+	if err != nil {
+		fmt.Println("Fetching error:", err)
+		resp.Body.Close()
+		pageCancel()
+		return
+	}
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Reading body: ", err)
+		resp.Body.Close()
+		pageCancel()
+		return
+	}
+	resp.Body.Close()
+
+	err = extracter.DownloadImages(pageCtx, c.imagesFolderName, c.db, b, j.URL)
+	if err != nil {
+		fmt.Println("DownloadImages Err", err)
+	}
+
+	if j.Depth >= maxDepth {
+		pageCancel()
+		return
+	}
+
+	links, err := extracter.ExtractLinks(pageCtx, b, j.URL)
+	if err != nil {
+		fmt.Println("ExtractLinks Err", err)
+		pageCancel()
+		return
+	}
+
+	for _, link := range links {
+		c.enqueue(job{URL: link, Depth: j.Depth + 1})
+	}
+
+	pageCancel()
 }
--- apps/imagecrawler/imagecrawler.go
+++ apps/imagecrawler/imagecrawler.go
@@ -2,31 +2,21 @@ package imagecrawler
 
 import (
 	"awesomeProject1/apps/imagecrawler/crawler"
-	"awesomeProject1/apps/imagecrawler/queue"
 	"awesomeProject1/config"
 	"awesomeProject1/db/postgres"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
-	"sync"
 )
 
 func RunCrawler() {
 	cfg := config.MustParseConfig()
-	var wg sync.WaitGroup
-	urlQueue := &queue.URLQueue{}
-	visited := &sync.Map{}
-
-	if err := os.MkdirAll("apps/imagecrawler/"+cfg.ImagesFolderName, os.ModePerm); err != nil {
-		fmt.Printf("Error creating image directory: %v\n", err)
-		os.Exit(1)
-	}
 
 	// Command line flags
 	urls := flag.String("urls", "https://www.slickerhq.com,https://www.ycombinator.com", "Comma-separated list of URLs to start crawling from")
-	timeoutInMinutes := flag.Uint("page-timeout", 100, "Crawling timeout per page duration in minutes")
-	maxWorkers := flag.Int("max-workers", 1000, "Maximum number of goroutines")
+	timeoutInMinutes := flag.Int("page-timeout", 100, "Crawling timeout per page duration in minutes")
+	maxWorkers := flag.Int("max-workers", 50, "Maximum number of goroutines")
 	flag.Parse()
 
 	if *urls == "" {
@@ -34,9 +24,6 @@ func RunCrawler() {
 		os.Exit(1)
 	}
 	startURLs := strings.Split(*urls, ",")
-	for _, url := range startURLs {
-		urlQueue.Enqueue(url, 0)
-	}
 
 	db, err := postgres.NewPostgresDatabase(cfg.PostgresDSN)
 	if err != nil {
@@ -46,17 +33,11 @@ func RunCrawler() {
 	defer db.Close()
 
 	c := crawler.NewCrawler(
-		urlQueue,
 		db,
+		*maxWorkers,
 		*timeoutInMinutes,
 		cfg.ImagesFolderName,
-		visited,
-		&wg,
 	)
 
-	wg.Add(*maxWorkers)
-	for i := 0; i < *maxWorkers; i++ {
-		go c.Crawl()
-	}
-	wg.Wait()
+	c.Start(startURLs)
 }
